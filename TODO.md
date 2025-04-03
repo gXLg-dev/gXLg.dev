04605fb4b637902089312d506820f4ab579aa9ac
gXLg <natgaev@gmail.com>
2025-04-04 01:22:07 +0200

    Privacy notice; whitelist register; improve securiy, mailing, pager; account settings;

--- TODO.md
+++ TODO.md
@@ -1,12 +1,21 @@
 * Add localization
+
 * Add reset password and change password possibility
-* Make required 2FA
+  * ~~Reset from outside~~
+  * ~~Reset from inside~~
+  * ~~Change password from inside~~
+  * ~~Update OTP from inside~~
+* ~~Make required 2FA~~
+
 * Stripe:
   * Generate contracts for an account
   * Billing and receipts
   * Cancellations
+
 * Texts:
   * Work out legal things needed to make a contract
   * Specify cancellation policy
   * Transparency: Show income
-  * Data privacy (minimal, I am not collecting any data)
+  * ~~Data privacy (minimal, I am not collecting any data)~~
+
+* ~~Fix imported file flags~~
--- index.js
+++ index.js
@@ -1,42 +1,64 @@
 const PROD = !!process.argv[2];
-module.exports = { PROD };
+const base = PROD ? "https://gxlg.dev" : "http://localhost:8080";
+module.exports = { PROD, base };
 
 const { nulls } = require("nulls");
 const nauth = require("nulls-auth");
 const nstate = require("nulls-state");
+const nturnstile = require("nulls-turnstile");
 
 const config = require("./config.json");
 const { getPage } = require("./lib/pages.js");
-const preprocessor = require("./lib/preprocessor.js");
+const preprocessor = require("./lib/preprocessor");
 
 (async () => {
 
   const auth = nauth({
     "path": "./db/users.badb",
-    "secret": config.jwt_secret,
-    "username": 254,
+    "username": "email",
     "secure": PROD,
+
     "verify": true,
-    "param": "email"
+    "reset": true,
+    "whitelist": true,
+    "otp": "gXLg.dev",
+
+    "admin": config.admin,
+    "adminCreate": require("./lib/auth/admin.js"),
+
+    "cloudflared": PROD,
+    "jwt": config.jwt,
+    "aes": config.aes,
+    "hmac": config.hmac
   });
 
   const state = nstate({
-    "secret": config.jwt_secret,
+    "jwt": config.jwt,
     "secure": PROD
   });
 
+  const turnstile = nturnstile({
+    ...config.turnstile,
+    "ignore": !PROD
+  });
+
   const server = await nulls({
+    "plugins": [auth, state, turnstile],
     "hook": async (req, res) => {
-      await auth(req, res);
-      state(req, res);
-      if (req.method == "GET") { req.page = getPage(req.path, req.auth); }
+      if (req.method == "GET") {
+        req.page = getPage(req.path, req.auth);
+        res.status(req.page.status);
+      }
+
+      req.admin = (req.auth == config.admin);
     },
     "nulls": "./site",
     "static": "./static",
     "uploads": false,
     "ready": () => console.log("Server up!"),
     "port": PROD ? parseInt(process.argv[2]) : 8080,
-    "https": PROD,
+    "forceHTTPS": PROD,
+    "domain": "gXLg.dev",
     preprocessor
   });
 
